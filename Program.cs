66d189667faa222bed76b098e50fe555f283c58e
Hector Flores <hecto932@gmail.com>
2019-03-28 17:58:11 -0600

    Creating an application console, generating number list and filtering

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace my_where
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+
+			// number list 1 to 1000
+      var listRandomNumbers = Enumerable.Range(1, 1000).ToList();
+
+			// eventList
+			var eventList = listRandomNumbers.Where(e => e % 2 == 0);
+
+			// print the first 3 numbers
+			for (int i = 0; i < 3; i++)
+			{
+					Console.WriteLine(eventList.ElementAt(i));
+			}
     }
+  }
 }
