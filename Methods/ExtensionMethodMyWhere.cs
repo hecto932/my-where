80e5e8e277d117fd521ea36575dbba9b50ceefc9
Hector Flores <hecto932@gmail.com>
2019-03-29 16:24:48 -0600

    Fixing public static class

--- Methods/ExtensionMethodMyWhere.cs
+++ Methods/ExtensionMethodMyWhere.cs
@@ -4,25 +4,30 @@ using System.Linq;
 
 namespace my_where.Methods
 {
-    public static class ExtensionMethodMyWhere
+  public static class ExtensionMethodMyWhere
+  {
+    private const int TIMES_TO_APPLY = 6;
+    public static int apply = TIMES_TO_APPLY;
+    public static IEnumerable<int> MyWhere(this IEnumerable<int> list, Func<int, bool> IsEven)
     {
-        public static IEnumerable<int> MyWhere(this IEnumerable<int> list, Func<int, bool>  IsEven, int apply = 0) {
-            var result = new List<int>();
-            int count = 0;
-            bool exit = false;
-            for(int i = 0; i < list.Count() && !exit; ++i) 
-            {
-                if (IsEven(list.ElementAt(i))) {
-                    result.Add(list.ElementAt(i));
-                    count++;
-                    if (apply != 0 && (count == apply)) {
-                        exit = true;
-                    }
-                }
-            }
-
-            Console.WriteLine($"MyWhere filter was applyed {count} times.");
-            return result;
+      var result = new List<int>();
+      int count = 0;
+      bool exit = false;
+      for (int i = 0; i < list.Count() && !exit; ++i)
+      {
+        if (IsEven(list.ElementAt(i)))
+        {
+          result.Add(list.ElementAt(i));
+          count++;
+          if (apply != 0 && (count == apply))
+          {
+            exit = true;
+          }
         }
+      }
+
+      //   Console.WriteLine($"MyWhere filter was applyed {count} times.");
+      return result;
     }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -9,33 +9,33 @@ namespace my_where
   {
     static void Main(string[] args)
     {
-			// Utilizando LINQ, generar una lista de los números del 1 al 1000.
+      // Utilizando LINQ, generar una lista de los números del 1 al 1000.
       var numberList = Enumerable.Range(1, 1000).ToList();
       Console.WriteLine($"numberList created with {numberList.Count} int elements...");
 
-			// Filtra la lista de números, tomando solo los que son pares
-			var evenList = numberList.Where(e => e % 2 == 0);
+      // Filtra la lista de números, tomando solo los que son pares
+      var evenList = numberList.Where(e => e % 2 == 0);
       Console.WriteLine($"Filtering all the even numbers using where function from linq...");
 
-			// Imprime solo los 3 primeros números de la lista de valores pares. (debe mostrar 2,4,6)
+      // Imprime solo los 3 primeros números de la lista de valores pares. (debe mostrar 2,4,6)
       Console.WriteLine($"Printing the first three numbers...");
-			printList(evenList, 3);
+      printList(evenList, 3);
 
       // A partir de este momento ya no puedes usar Where de LINQ;
       // reemplázala por un extension method local equivalente en funcionalidad. i.e. numeros.Where(...) ahora debe leerse como numeros.MyWhere(...)
-			var evenListMyWhere = numberList.MyWhere(e => e % 2 == 0, 6);
+      var evenListMyWhere = numberList.MyWhere(e => e % 2 == 0);
 
       // print the first 3 numbers
-			printList(evenListMyWhere);
+      printList(evenListMyWhere);
     }
 
     private static void printList(IEnumerable<int> list, int limit = 0)
     {
       limit = limit == 0 ? list.Count() : limit;
       for (int i = 0; i < limit; i++)
-			{
-					Console.WriteLine(list.ElementAt(i));
-			}
+      {
+        Console.WriteLine(list.ElementAt(i));
+      }
     }
   }
 }
